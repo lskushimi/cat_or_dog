a91045d26eb15bebc6cb14bbf0dd69e7164871d4
lskushimi <80935716+lskushimi@users.noreply.github.com>
2022-07-11 17:08:03 -0400

    Delete average_pet.m

--- average_pet.m
+++
@@ -1,7 +0,0 @@
-%This function takes in a data matrix X and a label
-%vector y and outputs the average cat image and average dog image.
-function [avgcat, avgdog] = average_pet(X,y)
-cat = find(y==-1);
-dog = find(y==1);
-avgcat = mean(X([transpose(cat)],:));
-avgdog = mean(X([transpose(dog)],:));
